f54304e608e1fd98d252884c7230aaeae18a165c
Seweryn <s.kreja@rewizja.net>
2016-01-15 12:01:58 +0100

    Theme:  Kamea

--- Kamea/functions.php
+++ Kamea/functions.php
@@ -73,9 +73,14 @@ function gs_theme_setup() {
 	 */
 	$content_width = apply_filters( 'content_width', 600, 430, 920, 1152 );
 	
-	//Custom Image Sizes
-	add_image_size( 'featured-image', 348, 448, TRUE );
-	
+	// Custom Image Sizes
+	//add_image_size( 'featured-image', 348, 448, TRUE );
+	add_image_size( 'featured-image', 211, 271, TRUE );
+
+	// Move image above post title in Genesis Framework 2.0
+	remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+	add_action( 'genesis_entry_header', 'genesis_do_post_image', 8 );
+
 	// Enable Custom Background
 	//add_theme_support( 'custom-background' );
 
@@ -150,13 +155,96 @@ function gs_theme_setup() {
 		'genesis-menus', 
 		array(
 			'primary'   => __( 'Primary Navigation Menu', CHILD_DOMAIN ), 
-			//'secondary' => __( 'Secondary Navigation Menu', CHILD_DOMAIN ),
+			'secondary' => __( 'Secondary Navigation Menu', CHILD_DOMAIN ),
 			'mobile'    => __( 'Mobile Navigation Menu', CHILD_DOMAIN ),
 		)
 	);
-	
+
+	//* Remove the entry meta in the entry footer (requires HTML5 theme support)
+	remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+
+	//* Remove the post meta function
+	remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
+
 	// Add Mobile Navigation
 	add_action( 'genesis_before', 'gs_mobile_navigation', 5 );
+
+
+	// add hook
+add_filter( 'wp_nav_menu_objects', 'my_wp_nav_menu_objects_sub_menu', 10, 2 );
+// filter_hook function to react on sub_menu flag
+function my_wp_nav_menu_objects_sub_menu( $sorted_menu_items, $args ) {
+	if ( isset( $args->sub_menu ) ) {
+		$root_id = 0;
+
+		// find the current menu item
+		foreach ( $sorted_menu_items as $menu_item ) {
+			if ( $menu_item->current ) {
+				// set the root id based on whether the current menu item has a parent or not
+				$root_id = ( $menu_item->menu_item_parent ) ? $menu_item->menu_item_parent : $menu_item->ID;
+				break;
+			}
+		}
+
+		// find the top level parent
+		if ( ! isset( $args->direct_parent ) ) {
+			$prev_root_id = $root_id;
+			while ( $prev_root_id != 0 ) {
+				foreach ( $sorted_menu_items as $menu_item ) {
+					if ( $menu_item->ID == $prev_root_id ) {
+						$prev_root_id = $menu_item->menu_item_parent;
+						// don't set the root_id to 0 if we've reached the top of the menu
+						if ( $prev_root_id != 0 ) $root_id = $menu_item->menu_item_parent;
+						break;
+					}
+				}
+			}
+		}
+		$menu_item_parents = array();
+		foreach ( $sorted_menu_items as $key => $item ) {
+			// init menu_item_parents
+			if ( $item->ID == $root_id ) $menu_item_parents[] = $item->ID;
+			if ( in_array( $item->menu_item_parent, $menu_item_parents ) ) {
+				// part of sub-tree: keep!
+				$menu_item_parents[] = $item->ID;
+			} else if ( ! ( isset( $args->show_parent ) && in_array( $item->ID, $menu_item_parents ) ) ) {
+				// not part of sub-tree: away with it!
+				unset( $sorted_menu_items[$key] );
+			}
+		}
+
+		return $sorted_menu_items;
+	} else {
+		return $sorted_menu_items;
+	}
+}
+
+	/** dave's genesis code for making the submenu  **/
+	add_action( 'wp_head', 'dc_add_tricky_menu' );
+	function dc_add_tricky_menu () {
+		// make all menus one level, or else dropdowns will be redundant in your submenu.
+		add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
+		// stick submenu under main menu. Of course you can use other hooks to put menu & submenu elsewhere.
+		add_action( 'genesis_after_header', 'dc_add_tricky_two' );
+	}
+
+	function my_wp_nav_menu_args( $args = '' ) {
+		$args['depth'] = 1;
+		return $args;
+	}
+
+	function dc_add_tricky_two () {
+		// this code is for retaining styling from the main menu, but giving you a class to re-style the menu if desired.
+		echo '<nav class="nav-primary submenu" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" style="display:none;"><div class="wrap">';
+		wp_nav_menu( array(
+			'theme_location' => 'primary',
+			'container' => 'false',
+			'menu_class' => 'genesis-nav-menu',
+			'sub_menu' => true
+		) );
+		echo '</div></nav>';
+	}
+	/**** end of dave's submenu code ****/
 	
 	//Enqueue Sandbox Scripts
 	add_action( 'wp_enqueue_scripts', 'gs_enqueue_scripts' );
@@ -286,3 +374,4 @@ function gs_do_after_entry() {
         );
  }
  }
+
